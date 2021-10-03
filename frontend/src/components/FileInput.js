66f63afac7437ba46f8e66f5740b41c00c6619a0
Hamza ARROUBAT <ht.arroubat@gmail.com>
2021-10-03 17:10:49 +0100

    styled pdf loader

--- frontend/src/components/FileInput.js
+++ frontend/src/components/FileInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FileUpload } from '@styled-icons/fa-solid/FileUpload';
 
 const Container = styled.div`
   display: grid;
@@ -7,13 +8,41 @@ const Container = styled.div`
   grid-template-rows: 0fr 0fr 0fr;
 `;
 
+const LabelContainer = styled.button``;
+
 const Label = styled.label`
-  white-space: nowrap;
-  margin-bottom: 10px;
+  cursor: pointer;
+  outline: none;
+  margin: 0;
+  display: grid;
+  grid-template-columns: max-content min-content;
+  gap: 0 0.5em;
+  align-items: center;
+  border: 1px solid hsl(214deg 100% 45%);
+  width: min-content;
+  padding: 0.5em 1em;
+  border-radius: 10px;
+  background: hsl(214deg 100% 45%);
+  color: hsl(0, 0%, 100%);
+  transition: color 0.2s;
+
+  svg {
+    cursor: pointer;
+    width: 15px;
+    color: hsl(0, 0%, 100%);
+
+    transition: color 0.2s;
+  }
+
+  transition: background 0.2s, color 0.2s;
+  :hover {
+    background: hsl(214deg 100% 40%);
+  }
 `;
 
 const InputContainer = styled.div`
   input {
+    display: none;
     padding: 1em 0.5em;
     border-radius: 3px;
     border: none;
@@ -51,7 +80,10 @@ function TextInput({
 }) {
   return (
     <Container hasError={!!errorMessage}>
-      <Label htmlFor={name}>{label}</Label>
+      <Label htmlFor={name}>
+        {label}
+        <FileUpload />
+      </Label>
       <InputContainer>
         {!!register ? (
           <input
